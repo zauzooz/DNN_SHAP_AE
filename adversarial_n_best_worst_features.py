fd01509371f0b0e2a3a64dd943afcbdda4c95449
zauzooz <nnt010802@gmail.com>
2023-07-14 16:55:34 +0700

    add plot n best vs n worst

--- adversarial_n_best_worst_features.py
+++ adversarial_n_best_worst_features.py
@@ -17,26 +17,42 @@ def adversarial_n_best_worst_features(
         n_best_features = selected_features[:i+1]
         static_features = [f for f in columns if f not in selected_features]
         adv_feature_val = adv_samples[n_best_features]
-        the_adv_sample = adv_samples.copy()
+        the_adv_sample = samples.copy()
         for f in n_best_features:
             the_adv_sample[f] = adv_samples[f].values
         acc_n_best_features.append(evaluate_metric(
             y_pred=model.predict(the_adv_sample),
             y_true=y_true
-        ))
+            )['accuracy_score'])
         
         # n worst features
         n_worst_features = selected_features[i+1:]
         static_features = [f for f in columns if f not in selected_features]
         adv_feature_val = adv_samples[n_worst_features]
-        the_adv_sample = adv_samples.copy()
+        the_adv_sample = samples.copy()
         for f in n_worst_features:
             the_adv_sample[f] = adv_samples[f].values
         acc_n_worst_features.append(evaluate_metric(
             y_pred=model.predict(the_adv_sample),
             y_true=y_true
-        ))
+            )['accuracy_score'])
         
     acc_n_worst_features.reverse()
     
     return (acc_n_best_features, acc_n_worst_features)
+
+def plot_n_best_vs_n_worst(
+    selected_features: list,
+    acc_n_best_important_features,
+    acc_n_worst_important_features
+):
+    import matplotlib.pyplot as plt
+    plt.plot(
+        [a+1 for a in range(len(selected_features))],
+        acc_n_best_important_features, 'red')
+
+    plt.plot(
+        [a+1 for a in range(len(selected_features))],
+        acc_n_worst_important_features , 'blue')
+
+    plt.show()
